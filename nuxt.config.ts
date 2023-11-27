ed81c37a6006644427e279fd5fc2d724137b3615
Joe.Chen <joechen@tracle-tw.com>
2023-11-27 10:41:25 +0000

    [dependence]: 新增 tailwindCSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
  * @Author: Joe.Chen
  * @Date: 2023-11-24 04:14:05
  * @LastEditors: Joe.Chen joechen@tracle-tw.com
- * @LastEditTime: 2023-11-24 08:47:27
+ * @LastEditTime: 2023-11-27 10:22:47
  * @Description: 
  */
 
@@ -17,6 +17,13 @@ export default defineNuxtConfig({
         }
       ],
     }
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
   },
   runtimeConfig: {
     public: {
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: Joe.Chen
+ * @Date: 2023-11-27 10:21:25
+ * @LastEditors: Joe.Chen joechen@tracle-tw.com
+ * @LastEditTime: 2023-11-27 10:21:26
+ * @Description: 
+ */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
